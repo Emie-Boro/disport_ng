acca4998908395f31090a3b4d966b82b3883c8f3
Emie-Trialer <emiedonmukumo@gmail.com>
2024-04-29 22:20:10 +0100

    Checking the .env file

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -27,7 +27,9 @@ const Signup = () => {
             console.log(err.message)
         })
     }
-    
+    const disabledBtn = (e) =>{
+        alert('Signup disabled by Admin...')
+    }
   return (
     <div className='container mx-auto'>
         <div className='p-5 flex flex-col lg:w-1/3'>
@@ -37,7 +39,7 @@ const Signup = () => {
                 <input type="password" placeholder='Password' value={password} onChange={(e)=> setPassword(e.target.value)} className='my-3 px-4 py-2 w-full' style={{border: '1px solid black'}}/>
             </div>
             <div>
-                <button onClick={handleSubmit} className={`bg-dark text-semi_light w-full py-3`}>Signup</button> 
+                <button onClick={handleSubmit} className="bg-dark text-semi_light w-full py-3" disabled>Signup</button> 
             </div>
         </div>
     </div>
